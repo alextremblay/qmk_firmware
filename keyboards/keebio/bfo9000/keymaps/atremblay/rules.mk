e57d0b646d89c70ae122859dda71f27a5e3fc4f9
Alex Tremblay <alex.tremblay@utoronto.ca>
2023-10-27 16:45:30 -0400

    .

---
+++ keyboards/keebio/bfo9000/keymaps/atremblay/rules.mk
@@ -0,0 +1,62 @@
+CONVERT_TO = elite_pi
+
+# Advanced Keycodes
+COMMAND_ENABLE = yes        # Commands for debug and configuration
+DYNAMIC_MACRO_ENABLE = no  # Dynamic Macro Support
+LEADER_ENABLE = no          # Enable leader key
+MOUSEKEY_ENABLE = yes       # Mouse keys
+PROGRAMMABLE_BUTTON_ENABLE = no # Programmable Buttons
+REPEAT_KEY_ENABLE = no	 # Enable Repeate Key
+
+# Software Features
+AUTO_SHIFT_ENABLE = yes      # Enable auto shift key
+AUTOCORRECT_ENABLE = no     # Enable autocorrect
+CAPS_WORD_ENABLE = yes       # Enable caps word
+COMBO_ENABLE = no           # Enable combos
+DIGITIZER_ENABLE = no       # Enable digitizer
+KEY_LOCK_ENABLE = no        # Enable key lock
+KEY_OVERRIDE_ENABLE = no    # Enable key override
+OS_DETECTION_ENABLE = yes    # Enable OS detection
+RAW_ENABLE = no             # Enable raw HID
+SECURE_ENABLE = no          # Enable keyboard locking
+SEND_STRING_ENABLE = yes   # Enable send string
+SEQUENCER_ENABLE = no       # Enable sequencer
+SWAP_HANDS_ENABLE = no      # Enable swap hands
+TAP_DANCE_ENABLE = no       # Enable tap dance
+DYNAMIC_TAPPING_TERM_ENABLE = no # Enable dynamic tapping term
+TRI_LAYER_ENABLE = yes       # Enable Tri-Layer keycodes
+UNICODE_COMMON = no			# Enable Unicode w/ common subsystem
+UNICODEMAP_ENABLE = no	  # Enable Unicode w/ map subsystem
+UCIS_ENABLE = no            # Enable Unicode w/ UCIS subsystem
+WPM_ENABLE = no             # Enable words per minute
+
+# Hardware Features
+QUANTUM_PAINTER_ENABLE = no # Enable Quantum Painter
+HD44780_ENABLE = no		 # Enable HD44780 LCD Display
+ST7565_ENABLE = no 		# Enable ST7565 LCD Display
+OLED_ENABLE = yes            # Enable OLED screen support
+
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+LED_MATRIX_ENABLE = no   	# Enable LED Matrix support
+RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE = no      # Enable RGB matrix.
+
+AUDIO_ENABLE = no           # Audio output
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+DIP_SWITCH_ENABLE = no      # DIP switch
+ENCODER_ENABLE = no         # Enable encoder support
+HAPTIC_ENABLE = no          # Haptic feedback
+JOYSTICK_ENABLE = no        # Enable joystick
+MIDI_ENABLE = no            # MIDI support
+POINTING_DEVICE_ENABLE = no # Enable pointing device (like a trackpad)
+PS2_MOUSE_ENABLE = no		 # Enable PS2 mouse
+PS2_ENABLE = no				 # Enable PS2
+SPLIT_KEYBOARD = yes 	  # Enable split keyboard
+
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+NKRO_ENABLE = yes            # Enable N-Key Rollover
+LTO_ENABLE = no             # Enable link-time optimization
+
+
